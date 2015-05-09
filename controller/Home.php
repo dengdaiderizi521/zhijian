49c88265bc487b99d9b8b245f425956f4d3aa8b4
至简PHP <php@php32.com>
2015-05-09 10:23:26 +0800

    至简PHP开源框架初学版

---
+++ controller/Home.php
@@ -0,0 +1,26 @@
+<?php
+/**
+  * 至简PHP开源框架
+  * 版本：至简初学版
+  * 官方网站：http://www.php32.com
+  * 日期：2015-05-01
+  * 示例文件，如果需要测试数据库，请解开调取model的注释，并在baseModel中设置数据库信息
+  */
+class Home extends BaseController{
+	function index(){
+
+		//调用模型有两种方法第一种
+		// $obj = $this->getModel('home');
+		// $data = $obj->getTestData();
+		
+		//第二种调用模型方法
+		// $this->getModel('home');
+		// $data = $this->home->getTestData();
+		
+		//设置模版变量
+		$this->set('variableName','至简PHP开源框架初学版');
+		//载入模版
+		$this->view('home');
+	}
+}
+?>
---
+++ model/HomeModel.php
@@ -0,0 +1,22 @@
+<?php
+/**
+  * 至简PHP开源框架
+  * 版本：至简初学版
+  * 官方网站：http://www.php32.com
+  * 官方论坛/帮助中心：http://bbs.php32.com
+  * 日期：2013-10-19
+  * 示例文件，如果执行请修改方法中的table_name为真实表名
+  */
+class HomeModel extends BaseModel {
+	function getTestData(){
+		$result = $this->query("SELECT * FROM table_name LIMIT 0,1");
+		if(!$result)  return false;
+		$data = array();
+		while ($row = mysql_fetch_assoc($result)) {	    //把查询结果重组成一个二维数组
+			$data[] = $row;
+		}
+		mysql_free_result($result);
+		return $data;
+	}
+}
+?>
