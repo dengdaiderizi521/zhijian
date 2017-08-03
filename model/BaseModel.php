248ace63d9578fce59b3af5bafdb48f120c163e7
程远皓 <chengyuanhao@51leho.cn>
2017-08-03 10:42:01 +0800

    更新mysql链接方式
    更新mysql为mysqli

--- model/BaseModel.php
+++ model/BaseModel.php
@@ -3,7 +3,7 @@
   * 至简PHP开源框架
   * 版本：至简初学版
   * 官方网站：https://github.com/dengdaiderizi521/zhijian
-  * 日期：2015-05-01
+  * 日期：2017-07-31
   */
 class BaseModel {
 
@@ -13,20 +13,20 @@ class BaseModel {
 	function __construct() {
 
 		//设置数据库服务器地址，用户名，密码并连接数据库
-		$this->_link = mysql_connect('mysql_host_name','mysql_username','mysql_username') or die('数据库服务器连接失败');
+		$this->_link = mysqli_connect('mysql_host_name','mysql_username','mysql_username') or die('数据库服务器连接失败');
 
 		//设置数据库字符集
-		mysql_query('set names utf8');
+		mysqli_query($this->_link, 'set names utf8');
 
 		//选择数据库
-		mysql_select_db('dbname');
+		mysqli_select_db($this->_link, 'dbname');
 	}
 
 	/**
 	 * 析构方法，释放资源，关闭数据库连接
 	 */
 	function __destruct(){
-		@mysql_close($this->_link);
+		@mysqli_close($this->_link);
 	}
 	
 	/**
@@ -35,7 +35,7 @@ class BaseModel {
 	  */
 	final public function query($sql) {
 		if(empty($this->_link)) return false;			//如果连接为空则返回FALSE
-		$result = mysql_query($sql);
+		$result = mysql_query($this->_link, $sql);
 		return $result;
 	}
 }
